3a037096eff4b15934e045214f55e6c6d19d50e4
Richard Garcia <richardgarciaph@gmail.com>
2018-07-08 23:55:31 +1200

    working on basic react STATE

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -3,14 +3,24 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+
+  state = {
+    persons: [
+      { name: 'Zabala', age: 27 },
+      { name: 'Garcia', age: 33 },
+      { name: 'Marcus', age: 29 }
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>this is really working!</p>
-        <Person personName='Zabala' personAge='27' />
-        <Person personName='Garcia' personAge='31'>my hobbies: racing, eating</Person>
-        <Person personName='Marcus' personAge='29'>
+        <button>Switch name</button>
+        <Person personName={this.state.persons[0].name} personAge={this.state.persons[0].age} />
+        <Person personName={this.state.persons[1].name} personAge={this.state.persons[1].age}>my hobbies: racing, eating</Person>
+        <Person personName={this.state.persons[2].name} personAge={this.state.persons[2].age}>
           children...
         </Person>
       </div>
--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -3,7 +3,7 @@ import React from 'react';
 const person = (props) => {
   return (
     <div>
-      <p>this is from linear component Person! :) <br/> my name is {props.personName}, i am {props.personAge} years old and my random age is {Math.floor(Math.random() * 30)}</p>
+      <p>this is from linear component Person! :) <br/> my name is {props.personName}, i am {props.personAge} years old <br/>and my random age is {Math.floor(Math.random() * 30)}</p>
       <p>{props.children}</p>
     </div>
   );
